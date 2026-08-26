6ca93f6be4a3e69077fefc86036b166145d122b4
Soham Dutta <sohamduttabwn@gmail.com>
2026-08-26 23:19:26 +0530

    Delete backend directory

--- backend/src/routes/demoRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getDemos, getDemoById } from '../controllers/demoController.js';
-
-const router = express.Router();
-
-router.get('/', getDemos);
-router.get('/:id', getDemoById);
-
-export default router;
